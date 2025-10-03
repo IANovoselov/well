91961cac427392be713bb4c988eec64f15529665
ia.novoselov <ia.novoselov@tensor.ru>
2025-10-03 17:12:33 +0500

    model struct

--- src/simulation/service_layer/__init__.py
+++ src/simulation/service_layer/__init__.py
@@ -1,1 +1,3 @@
+from matplotlib import pyplot as plt
+
 plt.rcParams.update({'font.size': 14})
--- src/simulation/service_layer/services.py
+++ src/simulation/service_layer/services.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from typing import Optional
 from datetime import date
 from matplotlib import pyplot as plt
+from dataclasses import dataclass
 
 PLOT_HIGHT = 5  # Высота графика
 PLOT_WIDTH = 10  # Ширина графика
@@ -22,27 +23,11 @@ class Scale:
 
 
 def build_plot(
-    x: pd.Series,
-    data: list[tuple[PlotData]],
-    title: str,
-    y_scale: Optional[list[Scale]] = None,
-    x_scale: Optional[Scale] = None,
-    mu=None,
+    data: dict,
 ):
     """Построитель графиков"""
 
-    if y_scale is None:
-        y_scale = []
-
-    # Масштаб по времени
-    x_min = 0
-    x_max = len(x)
-    if x_scale:
-        x_min = int(x_scale.min / dt)
-        x_max = int(x_scale.max / dt)
-    x = x[x_min:x_max]
-
-    fig, ax = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HIGHT))
+    _, ax = plt.subplots(figsize=(PLOT_WIDTH, PLOT_HIGHT))
 
     # Проинициализировать дополнительные шкалы
     axes = [ax]
@@ -52,7 +37,7 @@ def build_plot(
         _ax.spines.right.set_position(("axes", 1.15))
 
     plots = []
-    xlabel = r'Уровень шума, ±% от истинной величины'
+    xlabel = '$Время, сутки$'
 
     # Построить графики
     for ax_index in range(len(data)):
@@ -61,7 +46,21 @@ def build_plot(
 
         _ax = axes[ax_index]
 
-        for plot_data in data[ax_index]:
+        x = data[ax_index].get('x')
+        dt = data[ax_index].get('dt')
+        mu = data[ax_index].get('mu')
+        x_scale = data[ax_index].get('x_scale')
+        y_scale = data[ax_index].get('y_scale')
+
+        x_min = 0
+        x_max = len(x)
+        if x_scale:
+            x_min = int(x_scale.min / dt)
+            x_max = int(x_scale.max / dt)
+        x = x[x_min:x_max]
+
+        for plot_data in data[ax_index].get('data'):
+
             values = plot_data.data[x_min:x_max]
 
             (p,) = _ax.plot(x, values, plot_data.style, lw=plot_data.width, label=f"${plot_data.label}$")
@@ -75,15 +74,10 @@ def build_plot(
         max_limit = y_scale[ax_index].max if y_scale[ax_index].max is not None else max(maxes) * 1.1
 
         y_label = f"${', '.join([plot_data.label for plot_data in data[ax_index] if plot_data.label])}$"
-        y_label = '$Ошибка\\ идентификации$'
         if mu:
-            y_label += ', '
-            y_label += mu
-            mu = None
+            y_label += f', {mu}'
 
         _ax.set(ylim=(min_limit, max_limit), xlabel=xlabel, ylabel=y_label)
-
-    plt.title(title)
 
     ax.legend()
     # ax.legend(loc='upper center', bbox_to_anchor=(0.5, -0.12),
